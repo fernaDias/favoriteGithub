a0afa78605e3e000738b9119d8873740e13fdc42
Fernanda Dias <fernanda.dias.silva@accenture.com>
2025-02-18 08:57:22 -0300

    add concepts of imutabiliry and localstorage.

--- js/Favorites.js
+++ js/Favorites.js
@@ -7,11 +7,21 @@ export class Favorites {
   }
 
   load() {
-    this.entries = [
-      { login: "fernadias", public_repos: 76, followers: 86 },
-      { login: "MaykBrito", public_repos: 106, followers: 2000 },
-      { login: "diego3g", public_repos: 789, followers: 3000 },
-    ];
+    const entries = JSON.parse(localStorage.getItem("@github-favorites")) || [];
+    // this.entries = [
+    //   { login: "fernadias", public_repos: 76, followers: 86 },
+    //   { login: "MaykBrito", public_repos: 106, followers: 2000 },
+    //   { login: "diego3g", public_repos: 789, followers: 3000 },
+    // ];
+  }
+
+  delete(user) {
+    const filteredEntreis = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+
+    this.entries = filteredEntreis;
+    this.update();
   }
 }
 
@@ -37,6 +47,13 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user p").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
       row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onClick = () => {
+        const isOk = confirm("Are you sure you want to remove this user?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
 
       this.tbody.append(row);
     });
